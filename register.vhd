7e9bec8eea7a5d5414c92c73f891acf2ee3d5197
codebreaker7 <codebreaker7@mail.ru>
2018-07-12 12:47:07 +0300

    PSL assertion added

--- register.vhd
+++ register.vhd
@@ -13,15 +13,18 @@ end entity;
 
 architecture behav of reg is
   signal ds: std_logic := '0';
+-- psl default clock is rising_edge(clk);
+-- psl property check_val is always {d} |=> {q};
 begin
   process(all)
   begin
     if rst_n = '0' then
       ds <= '0';
-    elsif clk = '1' and clk'event then
+    elsif rising_edge(clk) then
       ds <= d;
     end if;
   end process;
   
   q <= ds;
+  -- psl assert check_val;
 end architecture;
